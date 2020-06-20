016b88224aa39b2c52d57df07861e6cd2cb80c3c
Ondrej Vondra <ondrej.vondra@live.com>
2020-06-20 17:07:13 +0200

    compilation

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IRRDIR=`pwd`/irrlicht
+
+CC=clang CXX=clang++ make -j3 -C irrlicht/source/Irrlicht/
+
+CC=clang CXX=clang++ cmake -S minetest -B minetest \
+-DCMAKE_PREFIX_PATH=/usr/lib/arm-linux-gnueabihf \
+-DRUN_IN_PLACE=TRUE \
+-DENABLE_GLES=1 \
+-DBUILD_SERVER=NO \
+-DIRRLICHT_SOURCE_DIR=$IRRDIR/source/Irrlicht/ \
+-DIRRLICHT_LIBRARY=$IRRDIR/lib/Linux/libIrrlicht.a \
+-DIRRLICHT_INCLUDE_DIR=$IRRDIR/include/ \
+-DCMAKE_EXE_LINKER_FLAGS="-lEGL -lGLESv2 -lGLESv1_CM -lGL" \
+-DEGL_INCLUDE_DIR=/usr/include/GLES/egl.h \
+-DEGL_LIBRARY=/usr/lib/arm-linux-gnueabihf/libEGL.so \
+-DOPENGLES2_INCLUDE_DIR=/usr/include/GLES2/gl2.h \
+-DOPENGLES2_LIBRARY=/usr/lib/arm-linux-gnueabihf/libGLESv2.so
+
+CC=clang CXX=clang++ make -j3 -C minetest
