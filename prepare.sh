207d3e3121e056cb5e500549d545e3f8d8734ceb
Ondrej Vondra <ondrej.vondra@live.com>
2020-06-20 18:03:58 +0200

    more packages

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 sudo apt install \
-  subversion
+  subversion \
+  build-essential \
+  clang \
+  libegl1-mesa-dev \
+  libgles2-mesa-dev \
+  cmake \
+  libx11-dev \
+  libjpeg-dev \
+  libpng-dev \
+  libbz2-dev \
+  libsqlite3-dev \
+  libleveldb-dev
